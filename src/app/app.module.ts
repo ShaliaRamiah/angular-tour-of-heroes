560bf785ea5b0a70ffe52f1e0684ee919a77f168
ShaliaRamiah <shalia.r20@gmail.com>
2023-04-21 16:17:56 +0200

    creating a feature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { AppComponent } from './app.component';
 
 import { HeroesComponent } from './heroes/heroes.component';
 
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
